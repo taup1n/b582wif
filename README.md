c452082c50a188c9c23af17a2c425203c51384eb
taup1n <taup1n@protonmail.com>
2017-12-13 23:38:30 -0800

    initial commit lol bitcoin

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # b582wif
-takes blockchain.info "address - rawb58" format and converts raw base58 keys into wallet import format (WIF)
+takes blockchain.info "address - rawb58" format and converts raw base58 keys into wallet import format (WIF).
+
+## dependencies
+pip install base58
+the rest should be stock p2.7.
+
+## usage
+./b582wif.py filename
+or
+./b582wif.py filename > b58.txt
+if you want to dump the output in a file.
+
+filename should be in format "address - raw b58 private key".
---
+++ b582wif.py
@@ -0,0 +1,32 @@
+# b582wif by taup1n
+# takes list of raw b58 blockchain.info private keys in 'addr - key' format 
+# transmutes into wif for easy import into more civilized wallets
+
+import sys, base58, hashlib, binascii, os.path
+
+def b2h(bytestr):
+    return ''.join( [ "%02X" % ord( x ) for x in bytestr ] ).strip()
+
+if len(sys.argv) < 2:
+	sys.exit("usage: ./" + sys.argv[0] + " filename")
+
+filename = sys.argv[1]
+
+if not os.path.isfile(filename):
+	sys.exit("file not found.")
+
+keypairs = []
+
+with open(filename) as f:
+	for line in f:
+		keypairs.append(line.strip().split('-'))
+for pair in keypairs:
+	try:
+		x,y = pair[0].strip(),pair[1].strip()
+		hexkey = "80" + b2h(base58.b58decode((y))).strip().lower()
+		r1 = hashlib.sha256(binascii.unhexlify(hexkey)).hexdigest()
+		r2 = hashlib.sha256(binascii.unhexlify(r1)).hexdigest()
+		keycsum = hexkey + r2[:8]
+		print base58.b58encode(binascii.unhexlify(keycsum))
+	except IndexError:
+		print "error in file format."
