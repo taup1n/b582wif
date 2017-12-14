b325b9fa59cc5b921fd57c0a965ffd23d309a693
taup1n <taup1n@protonmail.com>
2017-12-13 23:48:34 -0800

    hotfix and cleanup

--- b582wif.py
+++ b582wif.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # b582wif by taup1n
 # takes list of raw b58 blockchain.info private keys in 'addr - key' format 
 # transmutes into wif for easy import into more civilized wallets
@@ -5,10 +7,10 @@
 import sys, base58, hashlib, binascii, os.path
 
 def b2h(bytestr):
-    return ''.join( [ "%02X" % ord( x ) for x in bytestr ] ).strip()
+    return ''.join(["%02x" % ord(x) for x in bytestr]).strip()
 
 if len(sys.argv) < 2:
-	sys.exit("usage: ./" + sys.argv[0] + " filename")
+	sys.exit("usage: " + sys.argv[0] + " filename")
 
 filename = sys.argv[1]
 
@@ -23,7 +25,7 @@ with open(filename) as f:
 for pair in keypairs:
 	try:
 		x,y = pair[0].strip(),pair[1].strip()
-		hexkey = "80" + b2h(base58.b58decode((y))).strip().lower()
+		hexkey = "80" + b2h(base58.b58decode((y))).strip()
 		r1 = hashlib.sha256(binascii.unhexlify(hexkey)).hexdigest()
 		r2 = hashlib.sha256(binascii.unhexlify(r1)).hexdigest()
 		keycsum = hexkey + r2[:8]
